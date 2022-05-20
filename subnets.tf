ba939beba13f8e8b79a2372ecc06a119594e0a6e
ymadhu0930 <71345883+ymadhu0930@users.noreply.github.com>
2022-05-20 13:46:41 -0700

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -3,22 +3,3 @@ data "aws_availability_zones" "AZs" {
 }
 
 
-resource "aws_subnet" "vpc_subnets_DMZ" {
-  count = var.var_present && length(var.var_DMZ_subnets) >0 ? length(var.var_DMZ_subnets) : 0
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.var_DMZ_subnets[count.index]
-  availability_zone = data.aws_availability_zones.AZs.names[count.index]
-  tags = {
-    Name = "DMZ-Subnets"
-  }
-}
-
-resource "aws_subnet" "vpc_subnets_infra" {
-  count = var.var_present && length(var.var_Infra_subnets) >0 ? length(var.var_Infra_subnets) : 0
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.var_Infra_subnets[count.index]
-  availability_zone = data.aws_availability_zones.AZs.names[count.index]
-  tags = {
-    Name = "INFRA-Subnets"
-  }
-}
