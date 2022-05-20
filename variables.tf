ddddf7dc2a94a591abbfa920773b11883c3dac7b
ymadhu0930 <71345883+ymadhu0930@users.noreply.github.com>
2022-05-20 13:47:04 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,3 @@
 variable "var_cidr_block" {
 }
 
-variable "var_DMZ_subnets" {
-  type = list(string)
-}
-
-variable "var_Infra_subnets" {
-  type = list(string)
-}
-
-variable "var_present" {
-  default = "true"
-}
-
-variable "var_vpc_name" {}
