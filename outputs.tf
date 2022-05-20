24f09e46dfc9a8e2f8d6a7ce6848f8094ba673bf
ymadhu0930 <71345883+ymadhu0930@users.noreply.github.com>
2022-05-20 13:46:20 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,4 @@ output "output_vpc_cidr" {
   value = aws_vpc.main.cidr_block
 }
 
-output "output_infra_subnet_id"{
-  value = aws_subnet.vpc_subnets_infra.*.id
-}
 
-output "output_dmz_subnet_id"{
-  value = aws_subnet.vpc_subnets_DMZ.*.id
-}
